b876f6365e0a5c72e01d5262ea80038d273ea925
zaydulin <zaydulin.vagiz@gmail.com>
2026-02-27 21:40:45 +0300

    template

--- src/apps/moderation/urls.py
+++ src/apps/moderation/urls.py
@@ -3,5 +3,7 @@ app_name = 'moderation'
 from . import views
 
 urlpatterns = [
+    # Модерация
+    path("moderation/dashboard/", views.DashboardModerationView.as_view(), name="moderation_dashboard_list"),
 
 ]
